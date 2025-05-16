5ec47588e6474af056952d77b1d4db04166fbea9
Ruvikv <mattdaemontmf@gmail.com>
2025-05-16 19:57:27 -0300

    nueva estructura del profe

---
+++ frontend/src/app/app.routes.ts
@@ -0,0 +1,15 @@
+import { Routes } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+
+export const routes: Routes = [
+    {
+        path: '',
+        component: NavbarComponent
+    },
+    {
+        path: '**',
+        redirectTo: ''
+    }
+
+
+]
